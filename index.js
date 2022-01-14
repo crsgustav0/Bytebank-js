71c3185eafc1e2eb3ca9157420dcf57ac3f20154
Crs <cristian.dias@terraviva.agr.br>
2022-01-13 22:19:55 -0300

    feat: Centralização retorno de funções, arquivo 'index.js'

--- index.js
+++ index.js
@@ -12,23 +12,29 @@ class ContaCorrente {
     /*Definição de função, método da classe*/
     sacar(valor) {//Saque
         /*this.propriedade, refere-se a conta informada*/
-        if (this.#saldo >= valor) {
-            this.#saldo -= valor
-
-            console.log(`Valor sacado ${valor}`, `Saldo atual ${this.#saldo}`)
-        } else {
+        if (this.#saldo <= valor) {
             console.log('Saldo insuficiente')
+
+            /*Finaliza a execução da função via validação*/
+            return;
         }
+
+        this.#saldo -= valor
+        console.log(`Valor sacado ${valor}`, `Saldo atual ${this.#saldo}`)
     }
 
     depositar(valor) {//Deposito
-        if (valor > 0) {
-            this.#saldo += valor
-
-            console.log(`Valor depositado ${valor}`, `Saldo atual ${this.#saldo}`)
-        } else {
+        if (valor <= 0) {
             console.log('Valor incorreto')
+
+            /*Finaliza a execução da função via validação*/
+            return;
         }
+
+        /*this.propriedade, refere-se a conta informada*/
+        this.#saldo += valor
+
+        console.log(`Valor depositado ${valor}`, `Saldo atual ${this.#saldo}`)
     }
 }
 
