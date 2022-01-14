b36a752136c6e0cebba03760bf76c79796d21bf4
Crs <cristian.dias@terraviva.agr.br>
2022-01-13 22:05:38 -0300

    feat: Definições funções classe, arquivo 'index.js'

--- index.js
+++ index.js
@@ -3,27 +3,50 @@ class Cliente {
     /*Definição de atributos, propriedadas da classe */
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
-    rg;
+    #saldo = 0;
+
+    /*Definição de função, método da classe*/
+    sacar(valor) {//Saque
+        /*this.propriedade, refere-se a conta informada*/
+        if (this.#saldo >= valor) {
+            this.#saldo -= valor
+
+            console.log(`Valor sacado ${valor}`, `Saldo atual ${this.#saldo}`)
+        } else {
+            console.log('Saldo insuficiente')
+        }
+    }
+
+    depositar(valor) {//Deposito
+        if (valor > 0) {
+            this.#saldo += valor
+
+            console.log(`Valor depositado ${valor}`, `Saldo atual ${this.#saldo}`)
+        } else {
+            console.log('Valor incorreto')
+        }
+    }
 }
 
 /*Criação de um novo Cliente*/
 const primeiroCliente = new Cliente()
-const segundoCliente = new Cliente()
 
 /*Atribuição de propriedades*/
 primeiroCliente.nome = 'Gustavo'
 primeiroCliente.cpf = 12345678900
-primeiroCliente.agencia = 1001
-primeiroCliente.saldo = 0;
-primeiroCliente.rg = 134567820;
 
-segundoCliente.nome = 'Cristian'
-segundoCliente.cpf = 45678912368
-segundoCliente.agencia = 1001
-segundoCliente.saldo = 0
-segundoCliente.rg = 741852963;
+/*Criação de uma nova Conta Corrente*/
+const contaCorrentePrimeiroCliente = new ContaCorrente()
+contaCorrentePrimeiroCliente.agencia = 1001
 
-console.log(primeiroCliente)
-console.log(segundoCliente)
+/*Deposito*/
+contaCorrentePrimeiroCliente.depositar(100)
+/*Saque*/
+contaCorrentePrimeiroCliente.sacar(50)
+
+//console.log(primeiroCliente)
+console.log(primeiroCliente, contaCorrentePrimeiroCliente)
