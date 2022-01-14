1f05f6ce28a05e33286cb87871ec1691b1aa8db8
Crs <cristian.dias@terraviva.agr.br>
2022-01-13 21:04:21 -0300

    feat: Criação arquivos 'index.html' e '.index.js'

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const nomePrimeiroCliente = 'Gustavo'
+const cpfPrimeiroCliente = 12345678900
+const agenciaPrimeiroCliente = 1001
+const saldoPrimeiroCliente = 0
+
+const nomeSegundoCliente = 'Cristian'
+const cpfSegundoCliente = 45678912368
+const agenciaSegundoCliente = 1001
+const saldoSegundoCliente = 0
+
+console.log(nomePrimeiroCliente)
