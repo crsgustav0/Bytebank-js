5e66e88ccf5bd39f42de8ec0d0740d236463d61a
Crs <cristian.dias@terraviva.agr.br>
2022-01-13 21:14:37 -0300

    feat: Definição de classe e atraibuição de propriedadades, arquivo 'index.js'

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+/*Criação de classe Cliente */
+class Cliente {
+    /*Definição de atributos, propriedadas da classe */
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+}
 
-const nomePrimeiroCliente = 'Gustavo'
-const cpfPrimeiroCliente = 12345678900
-const agenciaPrimeiroCliente = 1001
-const saldoPrimeiroCliente = 0
+/*Criação de um novo Cliente*/
+const primeiroCliente = new Cliente()
+const segundoCliente = new Cliente()
 
-const nomeSegundoCliente = 'Cristian'
-const cpfSegundoCliente = 45678912368
-const agenciaSegundoCliente = 1001
-const saldoSegundoCliente = 0
+/*Atribuição de propriedades*/
+primeiroCliente.nome = 'Gustavo'
+primeiroCliente.cpf = 12345678900
+primeiroCliente.agencia = 1001
+primeiroCliente.saldo = 0;
+primeiroCliente.rg = 134567820;
 
-console.log(nomePrimeiroCliente)
+segundoCliente.nome = 'Cristian'
+segundoCliente.cpf = 45678912368
+segundoCliente.agencia = 1001
+segundoCliente.saldo = 0
+segundoCliente.rg = 741852963;
+
+console.log(primeiroCliente)
+console.log(segundoCliente)
