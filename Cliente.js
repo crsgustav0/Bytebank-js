7d46602c4fa9b2a56a4b8422b4d4d30c539f931e
Crs <cristian.dias@terraviva.agr.br>
2022-01-17 22:39:36 -0300

    feat: Criação construtor, arquivo 'Cliente.js'

--- Cliente.js
+++ Cliente.js
@@ -1,7 +1,23 @@
 //export  - Torna a classe acessível a outros diretorios do projeto
 
 export class Cliente {
-    /*Definição de atributos, propriedadas da classe */
-    nome;
-    cpf;
+  /*Definição de atributos, propriedadas da classe */
+  nome;
+
+  // _variavel - Indica o campo privado da classe
+  _cpf;
+
+  //Usado sem o '_' por se referenciar a sua chamada de outra classe
+  get cpf() {
+    /*
+      Retorna o valor do campo privado da classe 
+      */
+    return this._cpf
+  }
+
+  //Criação construtor classe, indica que para criar o objeto os parâmetros devem ser informados 
+  constructor(nome, cpf) {
+    this.nome = nome
+    this._cpf = cpf
+  }
 }
