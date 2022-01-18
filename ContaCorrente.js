8b6dd48c8345e83e1216ded4aa833d07eab50b27
Crs <cristian.dias@terraviva.agr.br>
2022-01-17 22:40:08 -0300

    feat: Criação funções 'getter' e 'setter' e construtores, arquivo 'ContaCorrente.js'

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,10 +1,47 @@
-//export  - Torna a classe acessível a outros diretorios do projeto
+import { Cliente } from "./Cliente.js";
 
+//export  - Torna a classe acessível a outros diretorios do projeto
 export class ContaCorrente {
     agencia;
-    cliente;
 
+    // _variavel - Indica o campo privado da classe
+    _cliente;
     _saldo = 0;
+
+    //Usado sem o '_' por se referenciar a sua chamada de outra classe
+    set cliente(novoValor) {
+        /*
+        Valida o objeto que chama a função, limitando seu acesso somente 
+        a sua classe sem 'Cliente', acessando o campo privado 
+        da classe 'ContaCorrente'
+        */
+        if (novoValor instanceof Cliente) {
+            /*
+            Atribui ao campo privado da classe o valor recebido
+            via parâmetro
+            */
+            this._cliente = novoValor
+        }
+    }
+
+    //Usado sem o '_' por se referenciar a sua chamada de outra classe
+    get cliente() {
+        /*
+        Atribui ao campo privado da classe o valor recebido via parâmetro
+        */
+        return this._cliente
+    }
+
+    //Usado sem o '_' por se referenciar a sua chamada de outra classe
+    get saldo() {
+        return this._saldo
+    }
+
+    //Criação construtor classe, indica que para criar o objeto os parâmetros devem ser informados 
+    constructor(agencia, cliente) {
+        this.cliente = cliente
+        this.agencia = agencia
+    }
 
     /*Definição de função, método da classe*/
     sacar(valor) {//Saque
@@ -50,7 +87,7 @@ export class ContaCorrente {
         const valorSacado = this.sacar(valor);
 
         conta.depositar(valorSacado);
-        
+
     }
 }
 
