7849448a19ff7313cc56b72e808da926b056df41
Crs <cristian.dias@terraviva.agr.br>
2022-01-17 22:40:37 -0300

    feat: Centralização criação objetos via construtores, arquivo 'index.js'

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 import { Cliente } from './Cliente.js'
 import { ContaCorrente } from './ContaCorrente.js'
 
-/*Criação de uma nova Conta Corrente*/
-const contaCorrentePrimeiroCliente = new ContaCorrente()
-contaCorrentePrimeiroCliente.agencia = 1001
-///*Criação de um novo Cliente*/
-contaCorrentePrimeiroCliente.cliente = new Cliente()
-/*Atribuição de propriedades*/
-contaCorrentePrimeiroCliente.cliente.nome = 'Gustavo'
-contaCorrentePrimeiroCliente.cliente.cpf = 12345678900
+/*Criação Conta Cliete*/
+const primeiroCliente = new Cliente("Cristian", 11122233309);
 
-/*Criação de uma nova Conta Corrente*/
-const contaCorrenteSegundoCliente = new ContaCorrente()
-/*Criação de um novo Cliente*/
-const segundoCliente = new Cliente()
-contaCorrenteSegundoCliente.agencia = 1002
-contaCorrenteSegundoCliente.cliente = new Cliente()
-/*Atribuição de propriedades*/
-contaCorrenteSegundoCliente.cliente.nome = 'Gustavo'
-contaCorrenteSegundoCliente.cliente.cpf = 12345678916
+/*Criação Conta Cliete*/
+const segundoCliente = new Cliente("Gustavo", 88822233309);
 
-///*Deposito*/
-contaCorrentePrimeiroCliente.depositar(500)
-///*Saque*/
-//contaCorrentePrimeiroCliente.sacar(50)
-//Transferência
-contaCorrentePrimeiroCliente.transferir(200, contaCorrenteSegundoCliente)
+/*Criação ContaCorrente*/
+const primeiraContaCorrente = new ContaCorrente(1001, primeiroCliente);
 
-console.log(contaCorrentePrimeiroCliente)
-console.log(contaCorrenteSegundoCliente)
+/*Deposito*/
+//primeiraContaCorrente.depositar(500)
+/*Saque*/
+//primeiraContaCorrente.sacar(50)
+
+/*Criação ContaCorrente*/
+const segundaContaCorrente = new ContaCorrente(1002, segundoCliente)
+
+//let valor = 200;
+//primeiraContaCorrente.transferir(valor, segundaContaCorrente)
+
+//segundaContaCorrente.saldo = 30000;
+console.log(primeiroCliente)
+console.log(segundoCliente)
+//console.log(segundaContaCorrente);
