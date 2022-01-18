23438417c084a50619f33e94d2162c244e24a951
Crs <cristian.dias@terraviva.agr.br>
2022-01-17 21:36:18 -0300

    feat: Instância direta componentes conta e transferência, arquivo 'index.js'

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 import { Cliente } from './Cliente.js'
 import { ContaCorrente } from './ContaCorrente.js'
 
-/*Criação de um novo Cliente*/
-const primeiroCliente = new Cliente()
-
-/*Atribuição de propriedades*/
-primeiroCliente.nome = 'Gustavo'
-primeiroCliente.cpf = 12345678900
-
 /*Criação de uma nova Conta Corrente*/
 const contaCorrentePrimeiroCliente = new ContaCorrente()
 contaCorrentePrimeiroCliente.agencia = 1001
+///*Criação de um novo Cliente*/
+contaCorrentePrimeiroCliente.cliente = new Cliente()
+/*Atribuição de propriedades*/
+contaCorrentePrimeiroCliente.cliente.nome = 'Gustavo'
+contaCorrentePrimeiroCliente.cliente.cpf = 12345678900
 
-/*Deposito*/
-contaCorrentePrimeiroCliente.depositar(100)
-/*Saque*/
-contaCorrentePrimeiroCliente.sacar(50)
+/*Criação de uma nova Conta Corrente*/
+const contaCorrenteSegundoCliente = new ContaCorrente()
+/*Criação de um novo Cliente*/
+const segundoCliente = new Cliente()
+contaCorrenteSegundoCliente.agencia = 1002
+contaCorrenteSegundoCliente.cliente = new Cliente()
+/*Atribuição de propriedades*/
+contaCorrenteSegundoCliente.cliente.nome = 'Gustavo'
+contaCorrenteSegundoCliente.cliente.cpf = 12345678916
 
-//console.log(primeiroCliente)
-console.log(primeiroCliente, contaCorrentePrimeiroCliente)
+///*Deposito*/
+contaCorrentePrimeiroCliente.depositar(500)
+///*Saque*/
+//contaCorrentePrimeiroCliente.sacar(50)
+//Transferência
+contaCorrentePrimeiroCliente.transferir(200, contaCorrenteSegundoCliente)
+
+console.log(contaCorrentePrimeiroCliente)
+console.log(contaCorrenteSegundoCliente)
