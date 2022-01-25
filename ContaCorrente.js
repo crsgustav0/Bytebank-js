a3c3a076a2105360db95f2914e00774413358e6b
Crs <cristian.dias@terraviva.agr.br>
2022-01-25 20:55:19 -0300

    feat: Centralização instância  propriedades classe 'Conta' via construtor, arquivo 'ContaCorrente.js'

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,50 +1,23 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-//export  - Torna a classe acessível a outros diretorios do projeto
-export class ContaCorrente {
+//  export  - Torna a classe acessível a outros diretorios do projeto
+
+//  extends - Utiliza as atribuições da classe Conta
+export class ContaCorrente extends Conta {
 
     static numeroContas = 0
 
-    //Usado sem o '_' por se referenciar a sua chamada de outra classe
-    set cliente(novoValor) {
-        /*
-        Valida o objeto que chama a função, limitando seu acesso somente 
-        a sua classe sem 'Cliente', acessando o campo privado 
-        da classe 'ContaCorrente'
-        */
-        if (novoValor instanceof Cliente) {
-            /*
-            Atribui ao campo privado da classe o valor recebido
-            via parâmetro
-            */
-            this._cliente = novoValor
-        }
-    }
-
-    //Usado sem o '_' por se referenciar a sua chamada de outra classe
-    get cliente() {
-        /*
-        Atribui ao campo privado da classe o valor recebido via parâmetro
-        */
-        return this._cliente
-    }
-
-    //Usado sem o '_' por se referenciar a sua chamada de outra classe
-    get saldo() {
-        return this._saldo
-    }
-
     //Criação construtor classe, indica que para criar o objeto os parâmetros devem ser informados 
-    constructor(agencia, cliente) {
-        this.cliente = cliente
-        this.agencia = agencia
-        this._saldo = 0
-
+    constructor(cliente, agencia) {
+        /*
+            Criação do objeto da classe Conta, necessária para a criação do objeto da classe ContaCorrente,
+            passando as propriedades via parâmetros
+        */
+        super(0, cliente, agencia)
         /*Referente a contabilização total da criação de contas, não somente a 
         sua instância no momento de sua criação*/
         ContaCorrente.numeroContas += 1
     }
 
-
 }
-
