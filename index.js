283b979c4caf31ca60fcf78fd608e411003d366d
Crs <cristian.dias@terraviva.agr.br>
2022-01-25 21:46:35 -0300

    style: Comentário funções, arquivo 'index.js'

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Cliente } from './Cliente.js'
+import { Conta } from './Conta.js'
 import { ContaCorrente } from './ContaCorrente.js'
 import { ContaPoupanca } from './ContaPoupanca.js'
 
@@ -11,9 +12,9 @@ const primeiroCliente = new Cliente("Cristian", 11122233309)
 const primeiraContaCorrente = new ContaCorrente(1001, primeiroCliente, agencia)
 
 /*Deposito*/
-primeiraContaCorrente.depositar(500)
+//primeiraContaCorrente.depositar(500)
 /*Saque*/
-primeiraContaCorrente.sacar(50)
+//primeiraContaCorrente.sacar(50)
 
 /*Criação ContaCorrente*/
 //const segundaContaCorrente = new ContaCorrente(1002, segundoCliente)
@@ -25,8 +26,8 @@ primeiraContaCorrente.sacar(50)
 //console.log(primeiroCliente)
 //console.log(primeiraContaCorrente)
 
-const contaPoupanca = new ContaPoupanca(50, primeiroCliente, agencia)
-contaPoupanca.sacar(10)
+//const contaPoupanca = new ContaPoupanca(50, primeiroCliente, agencia)
+//contaPoupanca.sacar(10)
 
-console.log(contaPoupanca)
-console.log(primeiraContaCorrente)
+//console.log(contaPoupanca)
+//console.log(primeiraContaCorrente)
