e332b7289ea0e6524e87b458a1fcb97bd3efd57e
Crs <cristian.dias@terraviva.agr.br>
2022-02-28 21:04:32 -0300

    feat: Validação autenticação, arquivo 'AuthSytem.js'

--- Auth/AuthSystem.js
+++ Auth/AuthSystem.js
@@ -2,7 +2,18 @@
 export class AuthSystem {
     /*Por se tratar de uma função específica somente a autenticação do usuário, 
     deve ser criada como privada*/
-    static login(funcionario, senha) {
-        return funcionario._senha == senha
+    static login(autenticavel, senha) {
+        if (AuthSystem.ehautenticavel(autenticavel)) {
+            return autenticavel.autenticar(senha)
+        }
+        return false;
+    }
+
+    static ehautenticavel(autenticavel) {
+        /*
+        in - Verifica a existencia de uma propriedade dentro do objeto
+        */
+        return 'autenticar' in autenticavel &&
+            autenticavel.autenticar instanceof Function
     }
 }
